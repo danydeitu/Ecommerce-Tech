db1de2ee462d9713b9b97d811b8bf538a8a62853
Daniel <danydeitu18@gmail.com>
2023-01-26 09:13:10 -0300

    clase 5

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -5,15 +5,18 @@ export const ItemCount = () => {
     const [contador, setContador] = useState(0)
             //Var       //Modificar var     //Estado inicial
 
-    const sumar = () =>  setContador(contador + 1) //contador = contador + 1
     const restar = () => setContador(contador - 1)
+    const sumar = () =>  setContador(contador + 1) //contador = contador + 1, - 1
+   
 
 
   return (
     <>
+       <button className="btn btn-light" onClick={() => restar()}>-</button>
+       {contador}
         <button className="btn btn-dark" onClick={() => sumar()}>+</button>
-            {contador}
-        <button className="btn btn-light" onClick={() => restar()}>-</button>
+        
+     
     </>
   )
 }
